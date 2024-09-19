d74302f53d2d249557cdd1879fab56a23c4b35d0
corecara.design.morimoto@gmail.com <corecara.design.morimoto@gmail.com>
2024-09-19 16:45:47 +0900

    Initial commit

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('9vhVHnOG2ySYldADpiacTQjwz4cEAEJW93dg3g/BCUGE8q4+WoEJfADJ1Oij0S/XDS6+PwaxHY4cCbxQcqcnSA1ragmegQJxcNax8qYXo51CiPPhWrvfzYFmIJAY7Ri9d7BO3uQLQdg/hXtYCq+bFgdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('')
+line_bot_api = LineBotApi('J8RVJvI8TzJYBctDJkLbkpHUI4d5G8o7I489CZ6PKSA1T/EXO0MOFy2GNpmSZNsdxzK42HmBH6gsXH20chmKOygiFmGRyOBQzc6B5LSsg8iSkTdkbl8W0Y00i6M6GUhdNxmXfLboyu3ma9g3nzeVfgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('acde0b7b4a5b9c4450af73a772bcd00d')
 
 users_current_question = {}  # ユーザーIDをキーとした現在の質問番号の記録
 users_answers = {}  # ユーザーIDをキーとした回答の記録
@@ -24,7 +24,7 @@ questions = [
     "あなたは今転職を考えていますか？",
     "計画を立てて目標に向かって頑張ることが得意ですか？",
     "新しい場面や状況に柔軟に対応できますか？",
-    "異性との出会いを求めていますか？",
+    "異性とので出会いを求めていますか？",
     "自分の強みや弱みを理解していますか？",
     "規則や手続きを重視しますか？",
     "自分の感情や考えを明確に述べることが得意ですか？",
@@ -32,20 +32,20 @@ questions = [
     "あなたは今の職場に不満を感じていますか？",
 ]
 
-# 質問に対応するMBTIタイプと点数を調整
+# 質問に対応するMBTIタイプと点数
 question_nurse_type_mapping = {
-    0: {"INFJ": 2, "ISFJ": 2, "ENFJ": 2, "ESFJ": 2, "INFP": 2, "ISFP": 2, "ENFP": 2, "ESFP": 2},
-    1: {"ENTP": 2, "INTP": 2, "ENTJ": 2, "INTJ": 2, "ENFP": 2, "INFP": 2, "ENTJ": 1, "INTJ": 1},
-    2: {"ISTJ": 2, "ESTJ": 2, "INTJ": 2, "ENTJ": 2, "ISFJ": 2, "INFJ": 1},  
-    3: {"ENTP": 5, "ESTJ": 1, "ISTJ": 1, "INFJ": 2, "INTP": 2},
-    4: {"INTJ": 2, "ENTJ": 2, "ISTJ": 2, "ESTJ": 2, "ISFJ": 1},  
-    5: {"ENFP": 2, "INFP": 2, "ENTP": 2, "INTP": 2, "ESFP": 1, "ISFP": 1},  
-    6: {"ESFP": 2, "ISFP": 2, "ENFP": 2, "INFP": 2},  
-    7: {"INTJ": 2, "INFJ": 2, "ISTJ": 1, "ISFJ": 1, "ENFP": 1, "ISFP": 1},  
-    8: {"ENFJ": 2, "ESFJ": 2, "INFJ": 2, "ISFJ": 2, "ESTJ": 1, "ISTJ": 1},  
-    9: {"ENTJ": 2, "INTJ": 2, "ENTP": 1, "INTP": 1, "ENFJ": 2, "ESFJ": 1},  
-    10: {"ENTP": 2, "INTJ": 1, "INFJ": 1, "ISFJ": 1, "ESTJ": 1},  
-    11: {"ENTP": 2, "INTJ": 1, "INFJ": 1, "ISFJ": 1, "ISFP": 1},  
+    0: {"INFJ": 2, "ISFJ": 2, "ENFJ": 2, "ESFJ": 2, "INFP": 2, "ISFP": 2, "ENFP": 1, "ESFP": 1},  # 感情に敏感
+    1: {"ENTP": 2, "INTP": 2, "ENTJ": 2, "INTJ": 2, "ENFP": 1, "INFP": 1},  # 新しい技術
+    2: {"ISTJ": 2, "ESTJ": 2, "INTJ": 2, "ENTJ": 2},  # 冷静さ
+    3: {"ENTP": 5, "ESTJ": 1, "ISTJ": 1, "ESTJ": 1},
+    4: {"INTJ": 2, "ENTJ": 2, "ISTJ": 2, "ESTJ": 2},  # 計画性
+    5: {"ENFP": 2, "INFP": 2, "ENTP": 2, "INTP": 2},  # 柔軟性
+    6: {"ESFP": 2, "ISFP": 2, "ENFP": 2, "INFP": 2},  # 社交性
+    7: {"INTJ": 2, "INFJ": 2, "ISTJ": 1, "ISFJ": 1},  # 自己認識
+    8: {"ENFJ": 2, "ESFJ": 2, "INFJ": 2, "ISFJ": 2},  # 励ます
+    9: {"ENTJ": 2, "INTJ": 2, "ENTP": 1, "INTP": 1},  # 明確な表現
+    10: {"ENTP": 2, "INTJ": 1, "INFJ": 1, "ISFJ": 1},  # 組織的
+    11: {"ENTP": 2, "INTJ": 1, "INFJ": 1, "ISFJ": 1},  # 組織的
 }
 
 @app.route("/callback", methods=['POST'])
@@ -95,34 +95,15 @@ def process_answer(user_id, text, reply_token):
         display_result(reply_token, users_answers[user_id], user_id)
 
 def display_result(reply_token, answers, user_id):
-    # 全質問に対応するMBTIタイプを一括で取得し、初期化
-    all_mbti_types = set()
-    for mapping in question_nurse_type_mapping.values():
-        all_mbti_types.update(mapping.keys())
-
-    mbti_scores = {mbti: 0 for mbti in all_mbti_types}  # 全てのMBTIタイプを初期化
+    mbti_scores = {mbti: 0 for mbti in question_nurse_type_mapping[0].keys()}
     
-    # 各質問の回答に基づいてスコアを加算
     for question_index, answer in enumerate(answers):
         if answer == "はい":
             for mbti, score in question_nurse_type_mapping[question_index].items():
                 mbti_scores[mbti] += score
 
     highest_score = max(mbti_scores.values())
-    
-    # 合計スコアがゼロの場合の処理を追加
-    if highest_score == 0:
-        line_bot_api.reply_message(reply_token, TextSendMessage(text="診断結果が生成できませんでした。もう一度お試しください。"))
-        return
-
-    # スコアが最大のMBTIタイプを選択
     top_mbti_types = [mbti for mbti, score in mbti_scores.items() if score == highest_score]
-    
-    # 結果が出ない場合のチェックを追加
-    if not top_mbti_types:
-        line_bot_api.reply_message(reply_token, TextSendMessage(text="診断結果が生成できませんでした。もう一度お試しください。"))
-        return
-
     selected_mbti_type = random.choice(top_mbti_types)
     result_message = f"あなたの看護師タイプは: {selected_mbti_type} です。結果を見るには、以下のボタンを押してください。"
 
@@ -135,5 +116,5 @@ def display_result(reply_token, answers, user_id):
     )
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
+    port = int(os.getenv("PORT", 5001))
     app.run(host="0.0.0.0", port=port)
