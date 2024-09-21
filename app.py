c51b0fa2bfc106d6cf51e46b7abe3b82004ca80c
corecara.design.morimoto@gmail.com <corecara.design.morimoto@gmail.com>
2024-09-21 10:36:59 +0900

    初回コミット: Flaskアプリのコードを追加

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
+from linebot.v3 import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage, QuickReply, QuickReplyButton, MessageAction
 import os
